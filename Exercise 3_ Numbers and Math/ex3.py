2ea7e20f682bca68f4d139f71c186f6b4b13d6fb
Nenny <mi.wiebe3@gmail.com>
2015-03-27 16:58:38 -0500

    Added initial files

---
+++ Exercise 3_ Numbers and Math/ex3.py
@@ -0,0 +1,23 @@
+print "I will now count my chickens."
+
+print "Hens", 25 + 30 / 6 # Calculates the number of Hens
+print "Roosters", 100 - 25 * 3 % 4 # Calculates the number of Roosters
+
+print "Now I will count the eggs:"
+
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 # Calculate the amount of eggs
+
+print "Is it true that 3 + 5 < 5 - 7?"
+
+print 3 + 2 < 5 - 7 # Since 5 > -2, this is false
+
+print "What is 3 + 2?", 3 + 2 
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's false."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2 # The next three lines will say if it is true or false
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
